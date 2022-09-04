404304bf046db5eb2a367dc51fceb31fbf0a94bc
Istvan Janetka <105000894+Istvan-Janetka@users.noreply.github.com>
2022-09-04 16:34:19 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import Header from './components/Header';
+import Logo from './assets/logo.svg';
+import Menu from './assets/menu.svg';
+import Item from './components/Item';
+import ModelS from './assets/models.jpg';
+import ModelX from './assets/modelx.jpg';
+import SolarPanel from './assets/solar_panel.jpg';
+import SolarRoof from './assets/solar_roof.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <Header
+        logo={Logo}
+        menu={Menu}
+      />
+      <div className="item-container">
+        <Item 
+          heading="Model S"
+          img={ModelS}
+        />
+
+        <Item 
+          heading="Model X"
+          img={ModelX}
+        />
+
+        <Item 
+          heading="Solar Panel"
+          img={SolarPanel}
+        />
+
+        <Item 
+          heading="Solar Roof"
+          img={SolarRoof}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
